1f6d5a70f0d8b869d23705b7fe3986d70b65b138
100lvlmaster <navinkodag@gmail.com>
2021-11-22 23:52:25 +0530

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Container from 'src/components/container.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('mounted');
+	});
+</script>
+
+<Container>
+	<div />
+</Container>
